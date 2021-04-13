cf4496d0fb1abf7d216aca58931bf0b9fc8a7d80
Chau Tran <cctran96@gmail.com>
2021-04-12 21:19:06 -0700

    commit tree 34cb1fa3efee367b0b9196baf35090ea4b64d12c

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const txt = document.getElementById('text')
-    txt.innerHTML = "This is really cool!"
-  })
+document.addEventListener('DOMContentLoaded', function() {
+  const text = document.querySelector('#text')
+  text.innerHTML = "This is really cool!"
+})
